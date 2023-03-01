8f6a716ad687ae080b8f883715d837c51b75c978
Aman Madhukar <amanmadhukar02@gmail.com>
2023-03-02 02:42:36 +0530

    Setup NX

--- apps/users/jest.config.ts
+++ apps/users/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'nestjs-jwt-authentication',
+  displayName: 'users',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/nestjs-jwt-authentication',
+  coverageDirectory: '../../coverage/apps/users',
 };
